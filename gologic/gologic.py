bc3abaf608d01bc5e8293dd087cab36cf9f16c73
Wonko-the-sane <bamcelroy@gmail.com>
2012-10-28 21:30:44 +0000

    refactored to use tuple coords

--- gologic/gologic.py
+++ gologic/gologic.py
@@ -11,10 +11,10 @@ class position_xy_out_of_range_error(go_logic_error): pass
 class position_occupied_error(go_logic_error): pass
 
 #function to return von neumann neighbourhood of a position
-def get_von_neumann(xy):
-    x = xy[0]
-    y = xy[1]
-    return [[x,y+1],[x,y-1],[x+1,y],[x-1,y]]
+def get_von_neumann(position):
+    x = position[0]
+    y = position[1]
+    return [(x,y+1),(x,y-1),(x+1,y),(x-1,y)]
 
 
 class game():
@@ -35,8 +35,8 @@ class game():
         
         
 class move():
-    def __init__(self,x,y,colour):
-        self.position = [x,y]
+    def __init__(self,position,colour):
+        self.position = position
         self.colour = colour
         self.adjacent_groups = []
                 
@@ -45,15 +45,18 @@ class board():
     def __init__(self,size):
         if size < 2: 
             raise bad_board_size_error,'Board size cannot be less than 2'
-        self.grid = [[None]*size for _ in range(size)]   
+        self.grid = {}
+        for x in range(size):
+            for y in range(size):
+                self.grid[(x,y)] = None   
     
     """add stone to board"""
-    def add_stone(self,x,y,colour,order):
-        if x > len(self.grid)-1 or x < 0 or y > len(self.grid[0])-1 or y < 0:
+    def add_stone(self,position,colour,order):
+        if position not in self.grid:
             raise position_xy_out_of_range_error,'position does not exist'
-        if self.grid[x][y] != None:
+        if self.grid[position] != None:
             raise position_occupied_error,'position is already occupied by a stone'  
-        self.grid[x][y] = stone(colour,order)
+        self.grid[position] = stone(colour,order)
 
 """ Stone class is very simple, just records colour and order placed"""  
 class stone():
@@ -65,23 +68,19 @@ class stone():
 
   
 class group():
-    def __init__(self,x,y,colour):
+    def __init__(self,position,colour):
         self.colour = colour
-        self.positions = [[x,y]]
+        self.positions = [position]
      
     """function to check if group is alive, if it finds one liberty it stops looking 
     if it completes the loop without finding any then the group is dead 
     The only relevant quantities of liberties a group can have are '0' or 'more than 0'"""  
     def am_i_alive(self,grid):
         for position in self.positions: #iterate through all positions in group
-            for i in get_von_neumann(position): #check von neumann neighbourhood of each one
-                if i[0] > 0 and i[1] > 0: #negative values will *not* be out of range as python interprets that as slicing, so gotta check for it here.
-                    if grid[i[0]] in grid: #check if neighbour cell is actually on board
-                        if grid[i[0]][i[1]] in grid[i[0]]:
-                            print i
-                            print grid[i[0]][i[1]]
-                            if grid[i[0]][i[1]] == None:
-                                return True
+            for vn_position in get_von_neumann(position): #check von neumann neighbourhood of each one
+                if vn_position in grid: #check if neighbour cell is actually on board
+                        if grid[vn_position] == None:
+                            return True
         return False
 
     def addposition(self):
@@ -93,8 +92,8 @@ class group():
 
 if __name__ == '__main__':
     testboard = board(13)
-    testboard.grid[1][0]= stone(0,0)
-    testboard.grid[0][1]= stone(0,0)
-    testgroup = group(0,0,0)
-    print testgroup.am_i_alive(testboard.grid) 
+    testboard.grid[(1,0)]= stone(0,0)
+    testboard.grid[(0,1)]= stone(0,0)
+    testgroup = group((0,0),0)
+    print testgroup.am_i_alive(testboard.grid)
     pass
--- gologic/gologictest.py
+++ gologic/gologictest.py
@@ -10,38 +10,39 @@ import gologic
 #test for my classes
 class class_tests(unittest.TestCase):pass
 
+
 #test my board class
 class board_tests(class_tests):
     
     #test new board instantiation    
     def test_new_board(self):
         self.testboard = gologic.board(19)
-        assert self.testboard.grid[0][0] == None
-        assert self.testboard.grid[18][18] == None
-        
+        assert self.testboard.grid[(0,0)] == None
+        assert self.testboard.grid[(18,18)] == None
+     
     #test bad board size    
     def test_bad_board_size(self):
         with self.assertRaises(gologic.bad_board_size_error):
             self.testboard = gologic.board(-5)
-            
+        
     #test out of range stone placement, less than zero and greater than gridsize in x and y    
     def test_position_xy_out_of_range(self):
         self.testboard = gologic.board(19)
         with self.assertRaises(gologic.position_xy_out_of_range_error):
-            self.testboard.add_stone(-1,0,1,15)
+            self.testboard.add_stone((-1,0),1,15)
         with self.assertRaises(gologic.position_xy_out_of_range_error):
-            self.testboard.add_stone(5,-1,1,15)
+            self.testboard.add_stone((5,-1),1,15)
         with self.assertRaises(gologic.position_xy_out_of_range_error):
-            self.testboard.add_stone(19,0,1,15)
+            self.testboard.add_stone((19,0),1,15)
         with self.assertRaises(gologic.position_xy_out_of_range_error):
-            self.testboard.add_stone(5,19,1,15)
+            self.testboard.add_stone((5,19),1,15)
 
     #test add_stone in already occupied position  
     def test_position_occupied(self):
         self.testboard = gologic.board(19)
         with self.assertRaises(gologic.position_occupied_error):
-            self.testboard.add_stone(0,0,1,15)
-            self.testboard.add_stone(0,0,0,16)
+            self.testboard.add_stone((0,0),1,15)
+            self.testboard.add_stone((0,0),0,16)
 
 
 #test my stone class
@@ -58,32 +59,44 @@ class stone_tests(class_tests):
         with self.assertRaises(gologic.bad_stone_order_error):
             self.teststone = gologic.stone(0,-2)
 
+
 #test my group class
 class group_tests(class_tests):
         
     #test new group    
     def test_new_group(self):
-        self.testgroup = gologic.group(3,5,0)
+        self.testgroup = gologic.group((3,5),0)
         assert self.testgroup.positions[0][0] == 3 , "x position should be 3"
         assert self.testgroup.positions[0][1] == 5 , "y position should be 5"
-        
+       
     #test am_i_alive function    
     def test_am_i_alive(self):
         self.testboard = gologic.board(13)
-        self.testgroup = gologic.group(3,3,0)
+        self.testgroup = gologic.group((3,3),0)
         assert self.testgroup.am_i_alive(self.testboard.grid) == True, "group should have more than 0 liberties"
-        
-        self.testgroup2 = gologic.group(0,0,0)
-        self.testboard.grid[1][0] = gologic.stone(1,2)
-        self.testboard.grid[0][1] = gologic.stone(1,3)
+         
+        self.testgroup2 = gologic.group((0,0),0)
+        self.testboard.grid[(1,0)] = gologic.stone(1,2)
+        self.testboard.grid[(0,1)] = gologic.stone(1,3)
         assert self.testgroup2.am_i_alive(self.testboard.grid) == False, "group should have 0 liberties"
-        
-        self.testgroup3 = gologic.group(10,10,0)
-        self.testboard.grid[11][10] = gologic.stone(1,2)
-        self.testboard.grid[9][10] = gologic.stone(1,3)
-        self.testboard.grid[10][11] = gologic.stone(1,4)
-        self.testboard.grid[10][9] = gologic.stone(1,5)
+     
+        self.testgroup3 = gologic.group((10,10),0)
+        self.testboard.grid[(11,10)] = gologic.stone(1,2)
+        self.testboard.grid[(9,10)] = gologic.stone(1,3)
+        self.testboard.grid[(10,11)] = gologic.stone(1,4)
+        self.testboard.grid[(10,9)] = gologic.stone(1,5)
         assert self.testgroup3.am_i_alive(self.testboard.grid) == False, "group should have 0 liberties"
+ 
+ 
+#test my move class
+class move_tests(class_tests):
+    
+    #test new move instantiation    
+    def test_new_move(self):
+        self.testmove = gologic.move((5,5),0)
+        assert self.testmove == None
+        assert self.testmove == None
+
 
 if __name__ == "__main__":
     unittest.main()    
